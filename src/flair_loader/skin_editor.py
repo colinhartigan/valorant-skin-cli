5078b1afd882e52d3470cd061a348a4fc7f16e0b
Colin <colinjoe9@gmail.com>
2021-07-05 17:44:57 -0400

    enable all skins for a weapon

--- src/flair_loader/skin_editor.py
+++ src/flair_loader/skin_editor.py
@@ -33,7 +33,8 @@ class Editor:
         if type_choice == "exit":
             return
         if type_choice == "Melee":
-            Editor.select_skin(skin_data, "2f59173c-4bed-b6c3-2191-dea9b58be9c7")
+            Editor.select_skin(
+                skin_data, "2f59173c-4bed-b6c3-2191-dea9b58be9c7")
         else:
             Editor.select_weapon(skin_data, type_choice)
 
@@ -41,8 +42,8 @@ class Editor:
     def select_weapon(skin_data, weapon_type):
 
         weapon_choices = [{
-                              "name": f"{data['display_name']} ({len(data['skins'])} skins, {len([skin for skin in data['skins'].keys() if data['skins'][skin]['enabled']])} enabled)",
-                              "value": uuid} for uuid, data in skin_data.items() if data['weapon_type'] == weapon_type]
+            "name": f"{data['display_name']} ({len(data['skins'])} skins, {len([skin for skin in data['skins'].keys() if data['skins'][skin]['enabled']])} enabled)",
+            "value": uuid} for uuid, data in skin_data.items() if data['weapon_type'] == weapon_type]
         weapon_choices.insert(0, {"name": "back", "value": "back"})
 
         weapon_choice = inquirer.select(
@@ -70,10 +71,13 @@ class Editor:
         }
         weapon_data = skin_data[weapon_choice]
 
-        skin_choices = [{
-                            "name": f"{'☑' if data['enabled'] else '☒'} [{tier_aliases[data['tier']['display_name']]}] {data['display_name']} ({len(data['levels'])} levels, {len(data['chromas'])} chromas)",
-                            "value": uuid} for uuid, data in weapon_data['skins'].items()]
+        skins_enabled = len([skin for uuid,skin in weapon_data['skins'].items() if skin['enabled'] and not 'Standard' in skin['display_name']])
+        total_skins = len([skin for uuid,skin in weapon_data['skins'].items() if not 'Standard' in skin['display_name']])
+
+        skin_choices = [
+            {"name": f"{'☑' if data['enabled'] else '☒'} [{tier_aliases[data['tier']['display_name']]}] {data['display_name']} ({len(data['levels'])} levels, {len(data['chromas'])} chromas)", "value": uuid} for uuid, data in weapon_data['skins'].items()]
         skin_choices.insert(0, {"name": "back", "value": "back"})
+        skin_choices.insert(1, {"name": ("disable" if skins_enabled == total_skins else "enable") + " all skins/chromas", "value": "change_all"})
 
         skin_choice = inquirer.select(
             message=f"[{weapon_data['display_name']}] select a skin to modify",
@@ -86,17 +90,32 @@ class Editor:
             else:
                 Editor.select_weapon(skin_data, weapon_data['weapon_type'])
             return
-
-        weapon_skin_data = weapon_data['skins'][skin_choice]
-        weapon_data['skins'][skin_choice] = Editor.set_skin_preferences(weapon_skin_data)
+        if skin_choice == "change_all":
+            Editor.change_all_skins_by_weapon(weapon_data,skins_enabled == total_skins)
+        else:
+            weapon_skin_data = weapon_data['skins'][skin_choice]
+            weapon_data['skins'][skin_choice] = Editor.set_skin_preferences(
+                weapon_skin_data)
 
         Loader.modify_skin_data(skin_data)
         Editor.select_skin(skin_data, weapon_choice)
 
     @staticmethod
+    def change_all_skins_by_weapon(weapon_data,disable):
+        for uuid, skin in weapon_data["skins"].items():
+            if not "Standard" in skin["display_name"]:
+                skin["enabled"] = not disable
+                for uuid, chroma in skin["chromas"].items():
+                    chroma["enabled"] = not disable
+            enabled_chromas = [chroma for _, chroma in skin['chromas'].items() if chroma['enabled']]
+            if len(enabled_chromas) == 0:
+                skin['chromas'][list(skin['chromas'].keys())[-1]]['enabled'] = True
+
+    @staticmethod
     def set_skin_preferences(skin_data):
         preferences = [
-            {'name': 'skin enabled', 'value': 'skin_enabled', 'enabled': skin_data['enabled']},
+            {'name': 'skin enabled', 'value': 'skin_enabled',
+                'enabled': skin_data['enabled']},
             Separator(),
         ]
 
@@ -121,7 +140,7 @@ class Editor:
             instruction='(space - toggle, enter - finish)'
         ).execute()
 
-        # clear out all old preferences 
+        # clear out all old preferences
         skin_data['enabled'] = False
         if len(skin_data['levels']) > 1:
             for uuid, level in skin_data['levels'].items():
@@ -134,21 +153,27 @@ class Editor:
         for preference in skin_preferences:
             if "level_" in preference or "chroma_" in preference:
                 if "level_" in preference:
-                    skin_data['levels'][preference.removeprefix('level_')]['enabled'] = True
+                    skin_data['levels'][preference.removeprefix(
+                        'level_')]['enabled'] = True
                 if "chroma_" in preference:
-                    skin_data['chromas'][preference.removeprefix('chroma_')]['enabled'] = True
+                    skin_data['chromas'][preference.removeprefix(
+                        'chroma_')]['enabled'] = True
 
             if preference == "skin_enabled":
                 skin_data['enabled'] = True
 
-                # check if any have 0 chromas/levels enabled then enable the top level
-        enabled_levels = [level for _, level in skin_data['levels'].items() if level['enabled']]
-        enabled_chromas = [chroma for _, chroma in skin_data['chromas'].items() if chroma['enabled']]
+        # check if any have 0 chromas/levels enabled then enable the top level
+        enabled_levels = [
+            level for _, level in skin_data['levels'].items() if level['enabled']]
+        enabled_chromas = [
+            chroma for _, chroma in skin_data['chromas'].items() if chroma['enabled']]
 
         if len(enabled_levels) == 0:
-            skin_data['levels'][list(skin_data['levels'].keys())[-1]]['enabled'] = True
+            skin_data['levels'][list(
+                skin_data['levels'].keys())[-1]]['enabled'] = True
 
         if len(enabled_chromas) == 0:
-            skin_data['chromas'][list(skin_data['chromas'].keys())[-1]]['enabled'] = True
+            skin_data['chromas'][list(
+                skin_data['chromas'].keys())[-1]]['enabled'] = True
 
         return skin_data
