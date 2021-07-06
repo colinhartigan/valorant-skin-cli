789ab12b8b39a7c510bff9c35575b020b9d8cf25
Colin <colinjoe9@gmail.com>
2021-07-05 21:05:47 -0400

    Update logging.py

--- src/utility/logging.py
+++ src/utility/logging.py
@@ -1,11 +1,13 @@
 import logging
+import os
+from .filepath import Filepath
 
 class Logger:
     
     @staticmethod
     def create_logger():
         # create logger
-        logging.basicConfig(filename='skincli.log',
+        logging.basicConfig(filename=Filepath.get_path(os.path.join(Filepath.get_appdata_folder(), 'skincli.log')),
                             filemode='w',
                             format='%(asctime)s.%(msecs)d %(name)s %(levelname)s %(message)s',
                             datefmt='%H:%M:%S',
