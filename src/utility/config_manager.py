40d794adb059e7b2e47414017ca87cbb804ada9b
Colin <colinjoe9@gmail.com>
2021-07-11 17:05:53 -0400

    added automatic config integrity checking

--- src/utility/config_manager.py
+++ src/utility/config_manager.py
@@ -2,6 +2,18 @@ import json
 import os
 from .filepath import Filepath
 
+default_config = {
+    "version": "v1.0.3",
+    "region": "na",
+    "async_refresh_interval": 5,
+    "skin_manager": {
+        "randomize_after_each_game": True
+    },
+    "meta": {
+        "onboarding_completed": False
+    }
+}
+
 class Config:
 
     @staticmethod
@@ -22,17 +34,38 @@ class Config:
         return Config.fetch_config()
 
     @staticmethod
+    def check_config():
+        # ???????
+        # my brain hurts
+        # i bet theres a way better way to write this but im just braindead
+        config = Config.fetch_config()
+        
+        def check_for_new_vars(blank,current):
+            for key,value in blank.items():
+                if not key in current.keys():
+                    current[key] = value
+                if key == "version": 
+                    current[key] = value
+                if isinstance(value,dict):
+                    check_for_new_vars(value,current[key])
+            
+        def remove_unused_vars(blank,current):
+            def check(bl,cur):
+                for key,value in list(cur.items()):
+                    if not key in bl.keys():
+                        del cur[key]
+                    if isinstance(value,dict) and key in list(cur.keys()):
+                        check(bl[key],value)
+
+            check(blank,current)
+            return current
+
+        check_for_new_vars(default_config,config)
+        config = remove_unused_vars(default_config,config)
+        Config.modify_config(config)
+
+    @staticmethod
     def create_blank_config():
-        config = {
-            "region": "na",
-            "async_refresh_interval": 5,
-            "skin_manager": {
-                "randomize_after_each_game": True
-            },
-            "meta": {
-                "onboarding_completed": False
-            }
-        }
         with open(Filepath.get_path(os.path.join(Filepath.get_appdata_folder(), 'config.json')), 'w') as f:
-            json.dump(config, f)
+            json.dump(default_config, f)
         return Config.fetch_config()
